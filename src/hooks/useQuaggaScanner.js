5a87a3503c1179756168801db4bec9670c0b6d1c
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-02-05 18:31:36 +0200

    work on scanner config

--- src/hooks/useQuaggaScanner.js
+++ src/hooks/useQuaggaScanner.js
@@ -13,17 +13,16 @@ export const useQuaggaScanner = (deviceId, onSetBarcode) => {
             width: 800,
             height: 1080,
             facingMode: 'environment',
-            aspectRatio: 16 / 9,
             deviceId,
           },
           area: {
-            top: '35%',
-            right: '10%',
-            left: '10%',
-            bottom: '35%',
+            top: '30%',
+            right: '8%',
+            left: '8%',
+            bottom: '30%',
           },
         },
-        numOfWorkers: 4,
+        numOfWorkers: 8,
         frequency: 20,
         locator: {
           patchSize: 'medium',
@@ -42,16 +41,13 @@ export const useQuaggaScanner = (deviceId, onSetBarcode) => {
       }
     );
 
-    const onBarcodeDetected = result => {
-      const video = document.querySelector('#scanner-container video');
-      if (!video) return;
-      
-      const canvas = document.createElement('canvas');
+    const video = document.querySelector('#scanner-container video');
+    const canvas = document.createElement('canvas');
+    const canvasCtx = canvas.getContext('2d');
 
+    const onBarcodeDetected = result => {
       canvas.width = video?.videoWidth;
       canvas.height = video?.videoHeight;
-
-      const canvasCtx = canvas.getContext('2d');
 
       canvasCtx.drawImage(video, 0, 0, canvas.width, canvas.height);
       onSetBarcode(result.codeResult.code, canvas.toDataURL('image/jpeg'));
