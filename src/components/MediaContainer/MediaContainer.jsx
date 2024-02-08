1c1b1d0ca05ace1db90ada74e89a57892ea4d185
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-02-08 18:27:11 +0200

    remove redundant commets

--- src/components/MediaContainer/MediaContainer.jsx
+++ src/components/MediaContainer/MediaContainer.jsx
@@ -2,18 +2,8 @@ import Scanner from '../Scanner/Scanner';
 import { useState, useRef } from 'react';
 import Loader from 'components/Loader/Loader';
 import scss from './MediaContainer.module.scss';
-// import { useVideoDevices } from 'hooks';
-
-// const styles = {
-//   width: 246,
-//   backgroundColor: 'yellow',
-//   minHeight: 10,
-//   marginTop: 5,
-//   overflow: 'hidden',
-// };
 
 const MediaContainer = ({ isStartScann, setIsScannStart, deviceId, onSetBarcode }) => {
-  // const { videoDevicesList } = useVideoDevices();
   const [isLoader, setIsLoader] = useState(false);
   const [mediaContainerBg, setMediaContainerBg] = useState('');
   const mediaContainerRef = useRef();
@@ -24,14 +14,6 @@ const MediaContainer = ({ isStartScann, setIsScannStart, deviceId, onSetBarcode 
       className={scss.mediaContainer}
       style={{ backgroundImage: `url(${mediaContainerBg})` }}
     >
-      {/* <ul style={{ overflow: 'scroll', height: '100%' }}>
-        {[...videoDevicesList].map((dev, idx) => (
-          <li key={idx} style={styles}>
-            <p>{dev.label}</p>
-            <p>{JSON.stringify(dev.getCapabilities(), null, '\n')}</p>
-          </li>
-        ))}
-      </ul> */}
       {isStartScann && (
         <Scanner
           onSetBarcode={onSetBarcode}
