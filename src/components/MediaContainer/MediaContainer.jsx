8f6a0b5845574597474b117f58490d38d03278b5
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-02-09 22:40:49 +0200

    Update MediaContainer.jsx

--- src/components/MediaContainer/MediaContainer.jsx
+++ src/components/MediaContainer/MediaContainer.jsx
@@ -1,19 +1,14 @@
 import Scanner from '../Scanner/Scanner';
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import Loader from 'components/Loader/Loader';
 import scss from './MediaContainer.module.scss';
 
 const MediaContainer = ({ isStartScann, setIsScannStart, deviceId, onSetBarcode }) => {
   const [isLoader, setIsLoader] = useState(false);
   const [mediaContainerBg, setMediaContainerBg] = useState('');
-  const mediaContainerRef = useRef();
 
   return (
-    <div
-      ref={mediaContainerRef}
-      className={scss.mediaContainer}
-      style={{ backgroundImage: `url(${mediaContainerBg})` }}
-    >
+    <div className={scss.mediaContainer} style={{ backgroundImage: `url(${mediaContainerBg})` }}>
       {isStartScann && (
         <Scanner
           onSetBarcode={onSetBarcode}
