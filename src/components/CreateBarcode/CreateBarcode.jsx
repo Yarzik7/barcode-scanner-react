a7737380006645fca571940d95302e1747c1c71b
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-03-02 14:37:25 +0200

    work on CreateBarcode

--- src/components/CreateBarcode/CreateBarcode.jsx
+++ src/components/CreateBarcode/CreateBarcode.jsx
@@ -1,10 +1,29 @@
 import Input from 'components/Input/Input';
+import { useState } from 'react';
 import scss from './CreateBarcode.module.scss';
 
 const CreateBarcode = () => {
+  const [barcode, setBarcode] = useState('');
+
+  const onChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'barcode':
+        setBarcode(value);
+        break;
+      default:
+        return;
+    }
+  };
+
   return (
     <div className={scss.createBarcodeContainer}>
-      <Input label="Barcode" placeholder="Enter barcode" />
+      <Input
+        label="Barcode"
+        placeholder="Enter barcode"
+        name="barcode"
+        value={barcode}
+        onChange={onChange}
+      />
     </div>
   );
 };
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,7 @@
 import { useId } from 'react';
 import scss from './Input.module.scss';
 
-const Input = ({ label = 'label', placeholder = 'input' }) => {
+const Input = ({ label = 'label', placeholder = 'input', name = 'input', onChange }) => {
   const inputId = useId();
 
   return (
@@ -9,7 +9,13 @@ const Input = ({ label = 'label', placeholder = 'input' }) => {
       <label htmlFor={inputId} className={scss.label}>
         {label}
       </label>
-      <input id={inputId} className={scss.input} placeholder={placeholder} />
+      <input
+        id={inputId}
+        name={name}
+        className={scss.input}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
     </div>
   );
 };
