3e9f7cb677964627d19d3781990acc517a108ca5
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-04-29 02:30:42 +0300

    Added message for camera list

--- src/components/Select/SelectOptionList/SelectOptionList.jsx
+++ src/components/Select/SelectOptionList/SelectOptionList.jsx
@@ -1,17 +1,13 @@
-import { SelectOption } from "../SelectOption/SelectOption";
-import scss from "./SelectOptionList.module.scss";
+import { SelectOption } from '../SelectOption/SelectOption';
+import scss from './SelectOptionList.module.scss';
 
 const SelectOptionList = ({ setSelect, onClick, list = [] }) => {
   return (
     <div className={scss.selectOptionListContainer}>
       <ul className={scss.selectOptionList}>
+        {!list.length && <li className={scss.listEmptyMessage}>Camera list is empty!</li>}
         {list.map((content, idx) => (
-          <SelectOption
-            key={idx}
-            content={content}
-            setSelect={setSelect}
-            onClick={onClick}
-          />
+          <SelectOption key={idx} content={content} setSelect={setSelect} onClick={onClick} />
         ))}
       </ul>
     </div>
